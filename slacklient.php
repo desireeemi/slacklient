d8d29eaa83e50b438e128ac4321c957dd3c185b5
Desire N'dri <desire.ndri@eemi.com>
2016-10-20 23:47:46 +0200

    j'arrive à rien

--- slacklient.php
+++ slacklient.php
@@ -16,33 +16,39 @@ function mes_assets() {
 }
 add_action( 'wp_enqueue_scripts', 'mes_assets' );
 
+function mon_action() {
+
+//    $ch = curl_init();
+//
+//    curl_setopt($ch, CURLOPT_URL, "https://www.example.com/");
+//    curl_setopt($ch, CURLOPT_POST, 1);
+//    curl_setopt($ch, CURLOPT_POSTFIELDS,
+//        "postvar1=value1&postvar2=value2&postvar3=value3");
+//
+//
+//
+//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//
+//    $curl = curl_exec ($ch);
+//
+//    if(!curl_exec($ch)){
+//        die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+//    }
+//
+//    var_dump($curl);
+//
+//    // Close request to clear up some resources
+//    curl_close ($ch);
+
+
+
+
+//    var_dump($curl);
+    var_dump($_POST);
+    json_encode($_POST);
+    var_dump($_POST);
+    die();
+}
 
 add_action( 'wp_ajax_post_slack', 'mon_action' );
 add_action( 'wp_ajax_nopriv_post_slack', 'mon_action' );
-
-function mon_action() {
-
-    $curl = curl_init();
-
-
-
-//    $_POST['messageContent'];
-//    $_POST['userName'];
-
-    curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => '"https://hooks.slack.com/services/T2R8ZMBQX/B2RS12C9K/EnQzYsuQQajCjnVaUJ',
-        CURLOPT_POST => 1,
-        CURLOPT_POSTFIELDS => array(
-            item1 => $_POST['userName'],
-            item2 => $_POST['messageContent']
-        )
-    ));
-// Send the request & save response to $resp
-    $resp = curl_exec($curl);
-// Close request to clear up some resources
-    curl_close($curl);
-
-    var_dump($resp);
-    die();
-}
