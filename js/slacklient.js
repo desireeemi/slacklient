460c7fd6dddfefccd358de8ef24ed2702fe995a0
Desire N'dri <desire.ndri@eemi.com>
2016-10-21 12:23:09 +0200

    L'envoi de message est fonctionnel !

--- js/slacklient.js
+++ js/slacklient.js
@@ -4,12 +4,16 @@ $("#sendMessage").on('submit', function (e) {
 
     e.preventDefault();
 
+    var messageContent = $('#messageContent').val();
+    var userName = $('#userName').val();
+    console.log(userName);
+
     jQuery.post(
         ajaxurl,
         {
             'action': 'post_slack',
-            'messageContent': 'coucou',
-            'userName': 'toto'
+            'messageContent': messageContent,
+            'userName': userName
         },
         function(response){
             console.log(response);
--- slacklient.php
+++ slacklient.php
@@ -18,21 +18,21 @@ add_action( 'wp_enqueue_scripts', 'mes_assets' );
 
 function mon_action() {
 
+
     $post = [
-        'username' => 'user1',
-        'password' => 'passuser1',
-        'gender'   => 1,
+        'username' => $_POST['userName'],
+        'text' => $_POST['messageContent'],
     ];
 //
 //    $test = json_encode($post);
 
     $ch = curl_init('https://hooks.slack.com/services/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');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-    
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+
 
 // execute!
-    $response = curl_exec($ch);
+//    $response = curl_exec($ch);
 
     if(!curl_exec($ch)){
         die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
@@ -40,17 +40,17 @@ function mon_action() {
 // close the connection, release resources used
     curl_close($ch);
 
-// do anything you want with your response
-    var_dump($response);
-
-
-
-
-//    var_dump($test);
-    var_dump($_POST);
-//    json_encode($_POST);
+//// do anything you want with your response
+//    var_dump($response);
+//
+//
+//
+//
+////    var_dump($test);
 //    var_dump($_POST);
-    die();
+////    json_encode($_POST);
+////    var_dump($_POST);
+//    die();
 }
 
 add_action( 'wp_ajax_post_slack', 'mon_action' );
