6bd7c28ae67fe9030f7a6c8968147c180414e5b9
Desire N'dri <desire.ndri@eemi.com>
2016-10-21 10:33:47 +0200

    Changement de requête CURL pour tester

--- slacklient.php
+++ slacklient.php
@@ -18,35 +18,38 @@ add_action( 'wp_enqueue_scripts', 'mes_assets' );
 
 function mon_action() {
 
-//    $ch = curl_init();
+    $post = [
+        'username' => 'user1',
+        'password' => 'passuser1',
+        'gender'   => 1,
+    ];
 //
-//    curl_setopt($ch, CURLOPT_URL, "https://www.example.com/");
-//    curl_setopt($ch, CURLOPT_POST, 1);
-//    curl_setopt($ch, CURLOPT_POSTFIELDS,
-//        "postvar1=value1&postvar2=value2&postvar3=value3");
-//
-//
-//
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//
-//    $curl = curl_exec ($ch);
-//
-//    if(!curl_exec($ch)){
-//        die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
-//    }
-//
-//    var_dump($curl);
-//
-//    // Close request to clear up some resources
-//    curl_close ($ch);
+//    $test = json_encode($post);
+
+    $ch = curl_init('https://hooks.slack.com/services/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');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    
+
+// execute!
+    $response = curl_exec($ch);
+
+    if(!curl_exec($ch)){
+        die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+    }
+// close the connection, release resources used
+    curl_close($ch);
+
+// do anything you want with your response
+    var_dump($response);
 
 
 
 
-//    var_dump($curl);
+//    var_dump($test);
     var_dump($_POST);
-    json_encode($_POST);
-    var_dump($_POST);
+//    json_encode($_POST);
+//    var_dump($_POST);
     die();
 }
 
